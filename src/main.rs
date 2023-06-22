46439f47319f2e681bf6afc6fd0f4e57e4a0b8ae
h1romas4 <h1romas4@gmail.com>
2023-06-22 19:35:00 +0900

    Separate line style logic

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
 mod config;
+mod line;
 
 use chrono::prelude::*;
 use std::fs;
 use zellij_tile::prelude::*;
-use zellij_tile_utils::style;
 
 use crate::config::PluginConfig;
+use crate::line::Line;
 
 // FIXME: DateTime backgorund color
 static DATETIME_BG_COLOR: (u8, u8, u8) = (32, 32, 32);
-
-static ARROW_SEPARATOR_1: &str = "";
-static ARROW_SEPARATOR_2: &str = "";
-static ARROW_SPACE: &str = " ";
 
 static INTERVAL_TIME: f64 = 1.0;
 
@@ -25,12 +22,7 @@ struct State {
     visible: bool,
     style: Style,
     style_update: bool,
-    fg_color: PaletteColor,
-    bg_color: PaletteColor,
-    datetime_bg_color: PaletteColor,
-    sp_1: String,
-    sp_2: String,
-    sp_3: String,
+    line: Line,
     config: PluginConfig,
 }
 register_plugin!(State);
@@ -108,34 +100,13 @@ impl ZellijPlugin for State {
     }
 
     fn render(&mut self, _rows: usize, cols: usize) {
-        // initialize cursol charctors
+        // update line style
         if self.style_update {
-            // pallet
-            self.fg_color = self.style.colors.fg;
-            self.bg_color = self.style.colors.bg;
-            self.datetime_bg_color = PaletteColor::Rgb(DATETIME_BG_COLOR);
-            let bg1 = self.bg_color;
-            let bg2 = self.datetime_bg_color;
-            // create charctor
-            let arrow = &style!(bg2, bg2).bold().paint(ARROW_SPACE).to_string();
-            let sep_1 = &style!(bg2, bg1).bold().paint(ARROW_SEPARATOR_1).to_string();
-            let sep_2 = &style!(bg1, bg2).bold().paint(ARROW_SEPARATOR_2).to_string();
-            self.sp_1 = String::new();
-            self.sp_1.push_str(sep_1);
-            self.sp_1.push_str(arrow);
-            self.sp_2 = String::new();
-            self.sp_2.push_str(arrow);
-            self.sp_2.push_str(sep_2);
-            self.sp_2.push_str(arrow);
-            self.sp_3 = String::new();
-            self.sp_3.push_str(arrow);
-            self.style_update = false;
+            self.line.update_style(self.style, DATETIME_BG_COLOR);
         }
 
         if let Some(now) = self.now {
-            // timezone
-            let timezone = self.timezone.to_string();
-            // date
+            // format date
             let date = format!(
                 "{year}-{month:02}-{day:02} {weekday}",
                 year = now.year(),
@@ -143,44 +114,14 @@ impl ZellijPlugin for State {
                 day = now.day(),
                 weekday = now.weekday(),
             );
-            // time
+            // format time
             let time = format!(
                 "{hour:02}:{minute:02}",
                 hour = now.hour(),
                 minute = now.minute(),
             );
-
-            // padding (support full width)
-            let timezone_len = timezone
-                .chars()
-                .map(|c| if c.is_ascii() { 1 } else { 2 })
-                .sum::<usize>();
-            let width = timezone_len + date.len() + time.len() + 9;
-            // There are cases where cols may be declared momentarily low at render time.
-            let padding: String = if cols as isize - width as isize > 0 {
-                let space = ARROW_SPACE.repeat(cols - width);
-                style!(self.fg_color, self.bg_color)
-                    .paint(space)
-                    .to_string()
-            } else {
-                String::new()
-            };
-
             // render
-            let timezone = style!(self.fg_color, self.datetime_bg_color)
-                .paint(&timezone)
-                .to_string();
-            let date = style!(self.fg_color, self.datetime_bg_color)
-                .paint(&date)
-                .to_string();
-            let time = style!(self.fg_color, self.datetime_bg_color)
-                .paint(&time)
-                .to_string();
-
-            print!(
-                "{}{}{}{}{}{}{}{}",
-                padding, self.sp_1, timezone, self.sp_2, date, self.sp_2, time, self.sp_3
-            );
+            print!("{}", self.line.render(cols, &self.timezone, &date, &time));
         }
     }
 }
